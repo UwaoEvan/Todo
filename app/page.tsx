041bce10346c63ae861f0d0ba005db5722df9ec4
UwaoEvan <uwaoevanz@gmail.com>
2024-06-07 14:44:36 +0300

    prisma

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,10 @@
+import db from "@/utils/db"
+
+const getTodos = async () => {
+  return await db.todo.findMany({})
+}
 
 export default function Home() {
+  const todos = getTodos();
   return <h1>Home</h1>
 }
